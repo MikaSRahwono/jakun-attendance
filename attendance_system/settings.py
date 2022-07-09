ba0ccd387b9076ecb3d81ca7c2bf355147c44263
MikaSRahwono <mikasuryof@outlook.com>
2022-07-09 15:59:18 +0700

    Bug fixing

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -33,7 +33,7 @@ if PRODUCTION:
     SECRET_KEY = os.getenv('SECRET_KEY_DJANGO')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["jakun-attendance.herokuapp.com", "*"]
 
